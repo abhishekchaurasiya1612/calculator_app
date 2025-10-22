b3b60b86b936e66d4491980baed2ba6a782ca806
Abhishek Chaurasiya <abhishekchaurasiya@Abhisheks-MacBook-Air.local>
2025-10-22 22:02:28 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+def add_num(num1,num2):
+    return num1+num2
+def sub_num(num1,num2):
+    return num1-num2
+def div_num(num1,num2):
+    return num1/num2
+def mul_num(num1,num2):
+    return num1*num2
+
+n1= int(input("Enter 1st number"))
+n2= int(input("Enter 2nd number"))
+c= ""
+print(add_num(n1,n2))
+print(sub_num(n1,n2))
+print(div_num(n1,n2))
+print(mul_num(n1,n2))
