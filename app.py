1e7b7d227cfcf664e704e2bcdad87899bbf32f44
Abhishek Chaurasiya <abhishekchaurasiya@Abhisheks-MacBook-Air.local>
2025-10-24 21:45:30 +0530

    Add while loop

--- app.py
+++ app.py
@@ -8,34 +8,34 @@ def mul_num(num1,num2):
     return num1*num2
 # def avg_num(num1,num2):
 #     return (num1+num2)/2
+while True:
+    print("Pls select operation:\n " \
+        "1. Add\n" \
+        "2. sub\n" \
+        "3. div\n" \
+        "4. mul\n" \
+        "5. avg\n" )
 
-print("Pls select operation:\n " \
-      "1. Add\n" \
-      "2. sub\n" \
-      "3. div\n" \
-      "4. mul\n" \
-      "5. avg\n" )
-
-select= int(input("Select a operation from 1,2,3,4: "))
-n1= int(input("Enter 1st number"))
-n2= int(input("Enter 2nd number"))
+    select= int(input("Select a operation from 1,2,3,4: "))
+    n1= int(input("Enter 1st number"))
+    n2= int(input("Enter 2nd number"))
 
 
-if select == 1:
-       print(n1, "+", n2, "=", \
-            add_num(n1,n2))
-    
-elif select == 2:
-       print(n1, "-", n2, "=", \
-            sub_num(n1,n2))
-elif select == 3:
-       print(n1, "/", n2, "=", \
-            div_num(n1,n2))
-elif select == 4:
-       print(n1, "-", n2, "=", \
-            mul_num(n1,n2))
-# elif select == 5:
-#        print(" (",n1, "+", n2,")", "/","2","=", \
-#             savg_num(n1,n2))
-else:
-         print("Invalid operation!") 
+    if select == 1:
+        print(n1, "+", n2, "=", \
+                add_num(n1,n2))
+        
+    elif select == 2:
+        print(n1, "-", n2, "=", \
+                sub_num(n1,n2))
+    elif select == 3:
+        print(n1, "/", n2, "=", \
+                div_num(n1,n2))
+    elif select == 4:
+        print(n1, "-", n2, "=", \
+                mul_num(n1,n2))
+    # elif select == 5:
+    #        print(" (",n1, "+", n2,")", "/","2","=", \
+    #             savg_num(n1,n2))
+    else:
+            print("Invalid operation!") 
